f94ca39179cb3d0c739be452043f2b985f8d5171
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2018-09-11 23:57:57 +0100

    set logo to more official looking

--- src/ParquetViewer/Model/TableRowView.cs
+++ src/ParquetViewer/Model/TableRowView.cs
@@ -2,21 +2,22 @@
 
 namespace ParquetViewer.Model
 {
-   public class TableRowView
-   {
-      private readonly Row _parquetRow;
+    public class TableRowView
+    {
+        /*private readonly Row _parquetRow;
 
-      public TableRowView(Row row)
-      {
-         this._parquetRow = row;
-      }
+        public TableRowView(Row row)
+        {
+           this._parquetRow = row;
+        }*/
 
-      public object this[int i]
-      {
-         get
-         {
-            return _parquetRow[i];
-         }
-      }
-   }
+        public object this[int i]
+        {
+            get
+            {
+                //return _parquetRow[i];
+                return "n/a";
+            }
+        }
+    }
 }
