e4b02008aea2aa28dd91fab1e0b75749e14cd15f
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2018-10-02 21:21:28 +0100

    set version manually, morons!

--- src/ParquetViewer/ParquetView.xaml.cs
+++ src/ParquetViewer/ParquetView.xaml.cs
@@ -1,46 +1,53 @@
-﻿using NetBox.Extensions;
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using NetBox.Extensions;
 using Parquet.Data;
+using Parquet.Data.Rows;
 using ParquetViewer.Model;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
 
 // The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace ParquetViewer
 {
-    public class FileViewModel
-    {
-        private ObservableCollection<TableRowView> _rows = new ObservableCollection<TableRowView>();
+   public sealed partial class ParquetView : UserControl
+   {
+      public ParquetView()
+      {
+         this.InitializeComponent();
+      }
 
-        public ObservableCollection<TableRowView> Rows => _rows;
+      public async Task DisplayAsync(StorageFile file)
+      {
+         FileGrid.Columns.Clear();
 
-        public FileViewModel()
-        {
+         Table table = await ParquetUwp.LoadAsync(file);
 
-        }
-    }
+         int i = 0;
+         foreach(Field f in table.Schema.Fields)
+         {
+            FileGrid.Columns.Add(new DataGridTextColumn
+            {
+               Header = f.Name,
+               Width = DataGridLength.SizeToCells,
+               Binding = new Binding
+               {
+                  Path = new PropertyPath("[" + i++ + "]")
+               }
+            });
+         }
 
-    public sealed partial class ParquetView : UserControl
-    {
-        public ParquetView()
-        {
-            this.InitializeComponent();
+         FileGrid.ItemsSource = table.Select(r => new TableRowView(r)).ToList();
 
-            ViewModel = new FileViewModel();
-
-        }
-
-        public FileViewModel ViewModel { get; set; }
-
-        public async Task DisplayAsync(StorageFile file)
-        {
-            FileGrid.Columns.Clear();
-        }
-    }
+         StatusText.Text = $"showing first {table.Count} records.";
+      }
+   }
 }
